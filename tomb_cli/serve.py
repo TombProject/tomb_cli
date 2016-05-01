d27fee2e2b66889a5b99017a33adeb16630bdecf
John Anderson <sontek@gmail.com>
2016-04-30 23:00:08 -0700

    load app config first

--- tomb_cli/serve.py
+++ tomb_cli/serve.py
@@ -6,8 +6,8 @@ from logging.config import dictConfig
 @click.command()
 @click.pass_context
 def serve(ctx):
-    server = load_server(ctx.obj.config_file, name=ctx.obj.server_env)
     log_config = load_logging_config(ctx.obj.config_file)
+    server = load_server(ctx.obj.config_file, name=ctx.obj.server_env)
     log_config['version'] = 1
     log_config['disable_existing_loggers'] = False
     dictConfig(log_config)
